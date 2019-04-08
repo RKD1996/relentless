e3b8e11600fef2acbee0fdb12841b1b4761b33db
RKD1996 <37078519+RKD1996@users.noreply.github.com>
2019-04-09 00:38:42 +0530

    som eminor changes in all the pages

--- public/js/init.js
+++ public/js/init.js
@@ -19,13 +19,30 @@ $(document).ready(function(){
   });
   $(window).scroll(function(){
     var a = $(window).scrollTop();
-    if(a > 500){
+    if(a > 180){
       $('.navbar').css({'background-color': 'black', 'boxbox-shadow': '0px 4px 8px 0px rgba(0,0,0,0.2)'});
     }
-    if(a < 300){
+    if(a < 180){
       $('.navbar').css({'background-color': 'transparent', 'boxbox-shadow': 'none'});
     }
   });
+  $('.info').click(function(){
+    $(window).load('./bin/about_us.html')
+  });
+  var myIndex = 0;
+carousal();
+
+function carousal() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}
+  x[myIndex-1].style.display = "block";
+  setTimeout(carousal, 2000); // Change image every 2 seconds
+}
 });
 //AIzaSyAR66kMcP3mI3frUS-gqs6MMYlkF0vu7kk map api code
 // autoplay()
